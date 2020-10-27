253733480f7e9b23b1781fa40fe7079db0602d3f
seyu <seyu@student.42seoul.kr>
2020-10-28 00:57:04 +0900

    headerfiles

---
+++ includes/mlx_color.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_color.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seyu <seyu@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/27 22:30:39 by seyu              #+#    #+#             */
+/*   Updated: 2020/10/27 22:41:56 by seyu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MLX_COLOR_H
+# define MLX_COLOR_H
+
+typedef	int	t_color;
+
+t_color	color_create_trgb(int t, int r, int g, int b);
+t_color	color_get_t(t_color trgb);
+t_color	color_get_r(t_color trgb);
+t_color	color_get_g(t_color trgb);
+t_color	color_get_b(t_color trgb);
+
+#endif
---
+++ includes/mlx_window.h
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_window.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seyu <seyu@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/27 22:56:20 by seyu              #+#    #+#             */
+/*   Updated: 2020/10/28 00:34:35 by seyu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MLX_WINDOW_H
+# define MLX_WINDOW_H
+
+# include <stddef.h>
+# include "mlx.h"
+# include "mlx_image.h"
+
+struct	s_window
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_image	**imglst_head;
+};
+typedef	struct s_window	t_window;
+
+/*
+**	-----------------------------------
+**	Window Initialization Functions
+**	-----------------------------------
+*/
+
+t_window	*window_new(int size_x, int size_y, char *title);
+int			window_delete(t_window **win);
+int			window_clear(t_window *win);
+
+/*
+**	-----------------------------------
+**	Image Initialization Functions
+**	-----------------------------------
+*/
+
+t_image		*window_new_image(t_window *win, int width, int height);
+int			window_delete_image(t_window *win, t_image *img);
+int			window_put_image(t_window *win, t_image *img);
+
+/*
+**	-----------------------------------
+**	Image List Functions
+**	-----------------------------------
+*/
+
+t_image		*window_find_last_image(t_window *win);
+t_image		*window_find_prev_image(t_window *win, t_image *img);
+t_image		*window_find_image(t_window *win, t_image *img);
+int			window_delete_image_all(t_window *win);
+
+#endif
