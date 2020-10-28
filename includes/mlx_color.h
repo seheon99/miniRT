8f64a279690c4c227e7444a25e1f82f558dc6587
seyu <seyu@student.42seoul.kr>
2020-10-29 00:32:34 +0900

    redefine t_color

--- includes/mlx_color.h
+++ includes/mlx_color.h
@@ -6,14 +6,14 @@
 /*   By: seyu <seyu@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/27 22:30:39 by seyu              #+#    #+#             */
-/*   Updated: 2020/10/28 01:19:39 by seyu             ###   ########.fr       */
+/*   Updated: 2020/10/28 23:35:36 by seyu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MLX_COLOR_H
 # define MLX_COLOR_H
 
-typedef	int	t_color;
+typedef	unsigned int	t_color;
 
 t_color	color_create_trgb(int t, int r, int g, int b);
 t_color	color_get_t(t_color trgb);
