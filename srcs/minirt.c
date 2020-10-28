5aebc003b2277d74bd86e19c4e283d27276298f2
seyu <seyu@student.42seoul.kr>
2020-10-29 00:32:10 +0900

    raytracing in1weekend : my first ppm image

--- srcs/minirt.c
+++ srcs/minirt.c
@@ -6,7 +6,7 @@
 /*   By: seyu <seyu@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 01:21:45 by seyu              #+#    #+#             */
-/*   Updated: 2020/10/28 02:18:31 by seyu             ###   ########.fr       */
+/*   Updated: 2020/10/29 00:16:43 by seyu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,34 @@
 
 /*
 **	-----------------------------------
-**	main function
+**	main functions
 **	-----------------------------------
 */
+
+static void
+	make_my_first_image(t_image *img, int image_width, int image_height)
+{
+	int	x;
+	int	y;
+	int	r;
+	int	g;
+	int	b;
+
+	y = 0;
+	while (y < image_height)
+	{
+		x = 0;
+		while (x < image_width)
+		{
+			r = (double)x / (image_width - 1) * 255.999;
+			g = (double)y / (image_height - 1) * 255.999;
+			b = 255.999 / 4;
+			image_pixel_put(img, x, y, color_create_trgb(128, r, g, b));
+			x += 1;
+		}
+		y += 1;
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -50,7 +75,7 @@ int	main(int argc, char **argv)
 	window = window_new(960, 540, "Hello, World!");
 	window_new_image(window, 960, 540);
 	image = window_find_last_image(window);
-	image_pixel_put(image, 480, 270, 0x00FF0000);
 	window_put_image(window, image, 0, 0);
+	make_my_first_image(image, 960, 540);
 	mlx_loop(window->mlx);
 }
